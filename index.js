120431ac4f0dc0e97cfaa837de713ef8590cc857
Dikardv <b3rt@narod.ru>
2021-03-26 13:02:25 +0300

    Delete the file at 'index.js'

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { createReadStream } from "fs";
-import http from "http";
-import crypto from "crypto";
-
-import appSrc from "./app.js";
-
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
